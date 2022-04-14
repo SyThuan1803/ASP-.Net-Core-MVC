788adfaf23e8bcd0169bbcdf566e830e00d3464a
THUẦN LÊ SỸ <19120387@student.hcmus.edu.vn>
2022-04-14 12:07:03 +0700

    Create project webAPI

---
+++ RestAPI_Project/RestAPI_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RestAPI_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
