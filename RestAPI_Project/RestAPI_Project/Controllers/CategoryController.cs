cb6747050dea029977922fa2c423a0a35b5da7c5
THUẦN LÊ SỸ <19120387@student.hcmus.edu.vn>
2022-04-16 00:55:28 +0700

    Edit and delete method, tempdata, partital view alert. Complete course!

--- RestAPI_Project/RestAPI_Project/Controllers/CategoryController.cs
+++ RestAPI_Project/RestAPI_Project/Controllers/CategoryController.cs
@@ -37,9 +37,71 @@ namespace RestAPI_Project_BookWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb= _db.Categories.Find(id);
+            //var categoryToDb= _db.Categories.FirstOrDefault(c=>c.Id==id);
+            //var categoryToDb = _db.Categories.SingleOrDefault(c => c.Id == id);
+            if (categoryFromDb==null)
+                return NotFound(); 
+            return View(categoryFromDb);   
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category edited successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryToDb= _db.Categories.FirstOrDefault(c=>c.Id==id);
+            //var categoryToDb = _db.Categories.SingleOrDefault(c => c.Id == id);
+            if (categoryFromDb == null)
+                return NotFound();
+            return View(categoryFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category obj)
+        {
+          
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+           
         }
     }
 }
