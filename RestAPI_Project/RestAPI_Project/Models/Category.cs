0390575b1162511420f8fed6402e2705787e9844
THUẦN LÊ SỸ <19120387@student.hcmus.edu.vn>
2022-04-15 19:04:03 +0700

    Create

--- RestAPI_Project/RestAPI_Project/Models/Category.cs
+++ RestAPI_Project/RestAPI_Project/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;  
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;  
 namespace RestAPI_Project_BookWeb.Models
 {
     public class Category
@@ -6,7 +7,9 @@ namespace RestAPI_Project_BookWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        public string Name { get; set; }   
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100 only!!")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; }= DateTime.Now;
 
